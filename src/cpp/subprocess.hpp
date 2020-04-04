08e4aaf83727241b0196d637ffaed3027c856e36
Ben Manashirov <benzakhar@gmail.com>
2020-04-04 17:00:44 -0400

    testing out github pages

--- src/cpp/subprocess.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "subprocess/basic_types.hpp"
-#include "subprocess/pipe.hpp"
-#include "subprocess/ProcessBuilder.hpp"
-#include "subprocess/shell_utils.hpp"
-#include "subprocess/environ.hpp"
